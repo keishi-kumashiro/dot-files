d5611a63b8cfe56c69b2d6032945e34013adbf75
Keishi Kumashiro <keishi.kumashiro@proton.me>
2022-09-06 01:12:04 -0400

    Add nvim config dir + subdirs

---
+++ nvim/lua/kk/opt.lua
@@ -0,0 +1,14 @@
+vim.opt.guicursor = ""
+
+vim.opt.number = true
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.mouse = "a"
+
