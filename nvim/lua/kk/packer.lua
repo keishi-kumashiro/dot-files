4a37858cc03dba8630908bb8d9614887115661d1
Keishi Kumashiro <keishi.kumashiro@proton.me>
2022-09-08 20:46:36 -0400

    Change identation style from 4 to 2 spaces

--- nvim/lua/kk/packer.lua
+++ nvim/lua/kk/packer.lua
@@ -1,18 +1,19 @@
 return require("packer").startup(function(use)
-    use "wbthomason/packer.nvim"
+  use "wbthomason/packer.nvim"
 
-    use "nvim-lua/plenary.nvim"
+  use "nvim-lua/plenary.nvim"
 
-    use {
-        "nvim-treesitter/nvim-treesitter",
-        run = ":TSUpdate"
-    }
-    use { "nvim-telescope/telescope.nvim", tag = "0.1.0" }
-    use { "nvim-telescope/telescope-fzf-native.nvim", run = "make" }
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate"
+  }
 
-    use {
-        "nvim-lualine/lualine.nvim",
-        requires = { "kyazdani42/nvim-web-devicons", opt = true }
-    }
+  use { "nvim-telescope/telescope.nvim", tag = "0.1.0" }
+  use { "nvim-telescope/telescope-fzf-native.nvim", run = "make" }
+
+  use {
+    "nvim-lualine/lualine.nvim",
+    requires = { "kyazdani42/nvim-web-devicons", opt = true }
+  }
 end)
 
