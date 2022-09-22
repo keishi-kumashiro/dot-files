ba640d6611368dab35c1aedc17794a982edc024d
Keishi Kumashiro <keishi.kumashiro@proton.me>
2022-09-22 17:14:19 -0400

    Add leader p keymap

--- nvim/lua/kk/keymap.lua
+++ nvim/lua/kk/keymap.lua
@@ -1,4 +1,7 @@
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
 vim.keymap.set("n", "<leader>pv", "<cmd>Ex<cr>")
+
 vim.keymap.set("n", "<leader>fd", "<cmd>lua require('telescope.builtin').find_files()<cr>")
 vim.keymap.set("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
 vim.keymap.set("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>")
