1c08c7a6002b41a35a97260d5a21f19a9cc0bd31
keishi-kumashiro <111834576+keishi-kumashiro@users.noreply.github.com>
2022-10-13 21:34:35 -0400

    Fix typo

--- nvim/lua/kk/opt.lua
+++ nvim/lua/kk/opt.lua
@@ -12,7 +12,7 @@ vim.opt.hlsearch = false
 
 vim.opt.scrolloff = 8
 
-vvim.opt.signcolumn = "yes"
+vim.opt.signcolumn = "yes"
 vim.api.nvim_set_hl(0, "SignColumn", {ctermbg=0, guibg=lightgrey})
 vim.opt.colorcolumn = "80"
 vim.api.nvim_set_hl(0, "ColorColumn", {ctermbg=0, guibg=lightgrey})
