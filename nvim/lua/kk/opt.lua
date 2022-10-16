198071f3fd8ace2ed6927d47912eabf5c7c3159c
keishi-kumashiro <111834576+keishi-kumashiro@users.noreply.github.com>
2022-10-15 23:35:25 -0400

    Comment out hi settings for sign and color columns

--- nvim/lua/kk/opt.lua
+++ nvim/lua/kk/opt.lua
@@ -13,9 +13,9 @@ vim.opt.hlsearch = false
 vim.opt.scrolloff = 8
 
 vim.opt.signcolumn = "yes"
-vim.api.nvim_set_hl(0, "SignColumn", {ctermbg=0, guibg=lightgrey})
+--vim.api.nvim_set_hl(0, "SignColumn", {ctermbg=0, guibg=lightgrey})
 vim.opt.colorcolumn = "80"
-vim.api.nvim_set_hl(0, "ColorColumn", {ctermbg=0, guibg=lightgrey})
+--vim.api.nvim_set_hl(0, "ColorColumn", {ctermbg=0, guibg=lightgrey})
 
 vim.opt.mouse = "a"
 
