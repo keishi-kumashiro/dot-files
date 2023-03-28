b4ea851300eef12bf04be08d241fc5e645fb9a47
Keishi Kumashiro <keishi.kumashiro@proton.me>
2023-03-28 16:33:40 -0400

    Update (mostly) keymaps

--- nvim/after/plugin/colour_scheme.lua
+++ nvim/after/plugin/colour_scheme.lua
@@ -1,5 +1,5 @@
 require("tokyonight").setup({
-  style = "night",
+  style = "moon",
   transparent = true,
   styles = {
     sidebars = "transparent",
--- nvim/lua/kk/keymap.lua
+++ nvim/lua/kk/keymap.lua
@@ -1,9 +1,17 @@
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
-vim.keymap.set("n", "<leader>pv", "<cmd>Ex<cr>")
+vim.keymap.set("n", "<leader>pv", "vim.cmd.Ex")
 
-vim.keymap.set("n", "<leader>fd", "<cmd>lua require('telescope.builtin').find_files()<cr>")
-vim.keymap.set("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
-vim.keymap.set("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>")
-vim.keymap.set("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>")
-
+vim.keymap.set("n", "<leader>sb", require("telescope.builtin").buffers)
+vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics)
+vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files)
+vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep)
+vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags)
+vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string)
+vim.keymap.set("n", "<leader>?",  require("telescope.builtin").oldfiles)
+vim.keymap.set("n", "<leader>/",  function()
+  require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown{
+    winblend = 18,
+    previewer = false
+  })
+end)
